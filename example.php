b74bab0df429bb778dc5d34b7479d5cad04cdcd2
Thomas Gates <tom@thebithead.com>
2013-04-16 15:34:07 -0400

    DB-2950 - change example to use a prepared statement

--- example.php
+++ example.php
@@ -1,14 +1,18 @@
 <?php 
 try {  
   $db = new PDO("nuodb:database=test@localhost;schema=Hockey", "dba", "goalie") or die;
-  $sql = "select * from hockey where NUMBER<12";
-  foreach ($db->query($sql) as $row) {
-     print_r ($row);
+  $number = 12;
+  $sql = "select * from HOCKEY where NUMBER < :number";
+  $stmt = $db->prepare($sql);
+  $stmt->bindParam(':number', $number, PDO::PARAM_INT);
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+  foreach ($result as $row) {
+     echo "NUMBER=" . $row["NUMBER"] . " NAME=" . $row["NAME"] . "\n";
   }
   $db = NULL;
 } catch(PDOException $e) {  
   echo $e->getMessage();  
-}
-$db = NULL;  
+} $db = NULL;  
 echo "done\n";
 ?>
